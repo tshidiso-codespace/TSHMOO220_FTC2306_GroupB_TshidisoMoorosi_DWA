c60985a85ba4b4d37822ecf918689496c4f6c318
tshidiso-codespace <tshidisomoorosi@gmail.com>
2023-10-04 21:34:30 +0200

    added IWA15 files

---
+++ DWA2/example.md
@@ -0,0 +1,5 @@
+# Arrays and Destructuring Challenge
+
+The scripts file contains code that extracts numbers from arrays contained in the "lists" array and combines all values into a single array called result. It orders the numbers in result by removing the biggest value amongst the last numbers in each array in "lists" and moving it to the "results" array.
+
+This was a challenge given to us as students by codespace, in which we had to edit an incorrect code so that it functions correctly
---
+++ DWA2/scripts.js
@@ -0,0 +1,73 @@
+// @ts-check
+
+/**
+ * @typedef {object} Data
+ * @property {Array<[string, Array<number>]>} lists - An array of lisy where each list contains a string and an array of numbers
+ */
+const data = {
+	lists: [
+		['first', [15, 11, 13, 7, 5]],
+		['second', [2, 6, 8, 4, 14, 12, 10]],
+		['third', [9, 3, 1]],
+	]
+}
+
+// Only edit below
+/**kkk
+ * @type {Array}
+ */
+let result = []
+let firstArray = data.lists[0][1]
+let secondArray = data.lists[1][1]
+let thirdArray = data.lists[2][1]
+
+/**
+ * 
+ * @returns {number} returns the largest mumber of the last elements of the lists
+ */
+const extractBiggest = () => {
+    let lastValues = [
+        firstArray[firstArray.length-1], 
+        secondArray[secondArray.length-1],
+        thirdArray[thirdArray.length-1]
+    ]
+    if (lastValues[0] === undefined) lastValues[0] = 0;
+    if (lastValues[1] === undefined) lastValues[1] = 0;
+    if (lastValues[2] === undefined) lastValues[2] = 0;
+
+	if (lastValues[0] === Math.max(...lastValues)) {
+        firstArray.pop();
+        
+    }
+        else if (lastValues[1] === Math.max(...lastValues)) {
+            secondArray.pop();
+        }
+
+	else {
+        thirdArray.pop();
+	}
+
+return(Math.max(...lastValues))
+}
+
+// Only edit above
+
+result.push(extractBiggest())
+result.push(extractBiggest())
+result.push(extractBiggest())
+result.push(extractBiggest())
+result.push(extractBiggest())
+
+result.push(extractBiggest())
+result.push(extractBiggest())
+result.push(extractBiggest())
+result.push(extractBiggest())
+result.push(extractBiggest())
+
+result.push(extractBiggest())
+result.push(extractBiggest())
+result.push(extractBiggest())
+result.push(extractBiggest())
+result.push(extractBiggest())
+
+console.log(result)  
